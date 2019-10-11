249289861d377a0a3b02dd7ddb2f385e286f8342
Toxa <p07a1330@ya.ru>
2019-10-11 17:12:12 +0300

    Total debug

---
+++ src/main/java/ru/bmstu/iu3/Editor.java
@@ -0,0 +1,39 @@
+package ru.bmstu.iu3;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class Editor extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public Editor(final PageParameters parameters) {
+		super(parameters);
+
+
+		Link toDirectoryInterface = new Link<Void>("toDirectoryInterface") {
+			@Override
+			public void onClick() {
+				setResponsePage(DirectoryInterface.class);
+			}
+		};
+		Link toTheory = new Link<Void>("toTheory") {
+			@Override
+			public void onClick() {
+				setResponsePage(Editor.class);
+			}
+		};
+		WebMarkupContainer menu = new WebMarkupContainer("menu");
+		menu.add(toTheory);
+		menu.add(toDirectoryInterface);
+		add(menu);
+		Form form = new Form("form");
+		TextArea textArea = new TextArea("text");
+		form.add(textArea);
+		add(form);
+
+	}
+}
---
+++ src/main/java/ru/bmstu/iu3/Theory.java
@@ -0,0 +1,6 @@
+package ru.bmstu.iu3;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class Theory extends WebPage {
+}
